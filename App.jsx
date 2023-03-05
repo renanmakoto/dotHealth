2c6832114958c477c0d83869393abaf0f222cc9c
user <renanmakoto@outlook.com>
2023-03-05 14:29:20 -0300

    test commit

--- App.jsx
+++ App.jsx
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 import Title from './src/Components/Title';
 import Main from './src/Components/Main';
 
