64495ddaceb945754e7b2f05526202c118bff58c
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2023-03-07 09:01:45 -0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './src/Components/Title';
+import Form from './src/Components/Form';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title />
+      <Form />
     </View>
   );
 }
