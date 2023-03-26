873254c857daa065557ae007985d22b55a166c55
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2023-03-26 10:38:59 -0400

    Add files via upload
    Style was added to the BMI information

--- src/Components/ResultBMI.jsx
+++ src/Components/ResultBMI.jsx
@@ -1,11 +1,32 @@
 import React from "react"
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet } from "react-native"
 
 export default function ResultBMI(props) {
     return(
-        <View>
-            <Text>{props.messageResultBMI}</Text>
-            <Text>{props.resultBMI}</Text>
+        <View style={styles.resultBMI}>
+            <Text style={styles.infotmation}>{props.messageResultBMI}</Text>
+            <Text style={styles.numberBMI}>{props.resultBMI}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    resultBMI: {
+        flex: 1,
+        marginTop: 15,
+        paddingTop: 60,
+        borderRadius: 50,
+        alignItems: "center",
+        width: "100%",
+    },
+    infotmation: {
+        fontSize: 18,
+        color: "#FF0043",
+        fontWeight: "bold",
+    },
+    numberBMI: {
+        fontSize: 48,
+        color: "#FF0043",
+        fontWeight: "bold",
+    }
+})
