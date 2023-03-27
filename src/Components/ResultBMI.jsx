dfa281fb5bd0122c1569b3090152b9a985fba8ad
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2023-03-27 10:30:53 -0400

    Add files via upload
    Share button was added and styled.

--- src/Components/ResultBMI.jsx
+++ src/Components/ResultBMI.jsx
@@ -1,9 +1,27 @@
 import React from "react"
-import { Text, View, StyleSheet } from "react-native"
+import { Text, View, StyleSheet, TouchableOpacity, Share } from "react-native"
 
 export default function ResultBMI(props) {
+    const onShare = async () => {
+        const result = await Share.share({
+            message: `My BMI today is: ${props.resultBMI}`
+        })
+    }
+
     return(
         <View style={styles.resultBMI}>
+            <View style={styles.boxShareButton}>
+                {props.resultBMI != null ?
+                    <TouchableOpacity 
+                        onPress={onShare}
+                        style={styles.shared}
+                    >
+                        <Text style={styles.sharedText}>Share</Text>
+                    </TouchableOpacity>
+                    :
+                        <View/>
+                }
+            </View>
             <Text style={styles.infotmation}>{props.messageResultBMI}</Text>
             <Text style={styles.numberBMI}>{props.resultBMI}</Text>
         </View>
@@ -28,5 +46,21 @@ const styles = StyleSheet.create({
         fontSize: 48,
         color: "#FF0043",
         fontWeight: "bold",
+    },
+    boxShareButton: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    shared: {
+        backgroundColor: "#1877F2",
+        borderRadius: 50,
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+    sharedText: {
+        color: "#FFFFFF",
+        fontWeight: "bold",
+        paddingHorizontal: 30,
     }
 })
