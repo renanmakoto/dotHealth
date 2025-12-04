a847bc622e3d8fabce3bfb816e9706dc024c075c
renanmakoto <renanmakoto@outlook.com>
2025-12-04 08:18:03 -0500

    Clean the code

---
+++ src/Components/common/index.js
@@ -0,0 +1,7 @@
+// Common UI components barrel export
+export { default as Button } from './Button'
+export { default as Card } from './Card'
+export { default as ErrorMessage } from './ErrorMessage'
+export { default as InputField } from './InputField'
+export { default as ScreenHeader } from './ScreenHeader'
+export { default as SectionLabel } from './SectionLabel'
