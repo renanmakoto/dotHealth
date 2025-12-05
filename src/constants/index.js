05edb5992abc2b7ffd2c8cff917436b05174a64f
renanmakoto <renanmakoto@outlook.com>
2025-12-05 08:18:13 -0500

    Clean code

---
+++ src/constants/index.js
@@ -0,0 +1,34 @@
+export const SCREENS = {
+  HOME: 'Home',
+  BMI_CALCULATOR: 'BMICalculator',
+  BMR_CALCULATOR: 'BMRCalculator',
+}
+
+export const APP_INFO = {
+  name: 'dotHealth',
+  author: 'dotExtension',
+  year: 2025,
+}
+
+export const GENDER_OPTIONS = {
+  MALE: 'male',
+  FEMALE: 'female',
+}
+
+export const MESSAGES = {
+  BMI: {
+    INITIAL: 'Fill in with height and weight',
+    RESULT: 'Your BMI is:',
+  },
+  BUTTONS: {
+    CALCULATE: 'Calculate',
+    CALCULATE_AGAIN: 'Calculate again',
+    CALCULATE_BMR: 'Calculate BMR',
+  },
+  HISTORY: {
+    EMPTY: 'Your history will appear here after the first calculation.',
+    TITLE: 'Recent results',
+  },
+}
+
+export * from './theme'
